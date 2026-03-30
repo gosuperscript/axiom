1cd4130692fb0b6cd25edf64b3bbda7fafff5da1
Robert van Steen <mail@robertvansteen.nl>
2026-03-30 16:09:34 +0200

    fix: revert phpstan update

--- src/Operators/HasOverloader.php
+++ src/Operators/HasOverloader.php
@@ -5,9 +5,8 @@ declare(strict_types=1);
 namespace Superscript\Axiom\Operators;
 
 use Superscript\Monads\Result\Result;
+
 use function Psl\Type\mixed_vec;
-use function Psl\Type\vec;
-use function Psl\Type\string;
 use function Superscript\Monads\Result\Ok;
 
 final readonly class HasOverloader implements OperatorOverloader
@@ -20,9 +19,7 @@ final readonly class HasOverloader implements OperatorOverloader
     /** @return Result<bool, never> */
     public function evaluate(mixed $left, mixed $right, string $operator): Result
     {
-        $left = vec(string())->coerce(mixed_vec()->coerce($left));
-        $right = vec(string())->coerce(is_array($right) ? $right : [$right]);
-
-        return Ok(array_intersect($right, $left) === $right);
+        $left = mixed_vec()->coerce($left);
+        return Ok(is_array($right) ? array_intersect($right, $left) === $right : in_array($right, $left));
     }
 }
--- src/Operators/IntersectsOverloader.php
+++ src/Operators/IntersectsOverloader.php
@@ -5,8 +5,7 @@ declare(strict_types=1);
 namespace Superscript\Axiom\Operators;
 
 use Superscript\Monads\Result\Result;
-use function Psl\Type\vec;
-use function Psl\Type\string;
+
 use function Superscript\Monads\Result\Ok;
 
 class IntersectsOverloader implements OperatorOverloader
@@ -22,8 +21,8 @@ class IntersectsOverloader implements OperatorOverloader
      */
     public function evaluate(mixed $left, mixed $right, string $operator): Result
     {
-        $left = vec(string())->coerce(is_array($left) ? $left : [$left]);
-        $right = vec(string())->coerce(is_array($right) ? $right : [$right]);
+        $left = is_array($left) ? $left : [$left];
+        $right = is_array($right) ? $right : [$right];
 
         return Ok(count(array_intersect($left, $right)) > 0);
     }
